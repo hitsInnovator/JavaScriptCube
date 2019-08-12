725f901525d8f4d8cfbe25095c02b1b7e2b935d0
Hitesh Panchal <hitesh035@gmail.com>
2019-08-12 18:08:26 +0530

    Add folders for prototype and array functions

---
+++ prototype/index.js
@@ -0,0 +1,42 @@
+function Employee (name) {
+    this.name = name;
+    // step:1
+    // this.getName = function () {
+    //     return this.name;
+    // }
+    // step: 3
+    // var employee = Object.create (Employee.prototype);
+    // employee.name = name;
+    //return employee;
+}
+
+// step:2
+// Employee.getName = function () {
+//     return this.name;
+// }
+
+Employee.prototype.getName = function () {
+    return this.name;
+}
+
+var emp1 = new Employee ("Hitesh");
+var emp2 = new Employee ("Pinky");
+
+console.log (emp1.getName (), emp2.getName ());
+////////////////////////
+
+function Vehicle (name, type) {
+    this.name = name;
+    this.type = type;
+}
+
+Vehicle.prototype.getName = function () {
+    return this.name;
+}
+
+Vehicle.prototype.getType = function () {
+    return this.type;
+}
+
+var car = new Vehicle ('Hyndai', 'car');
+console.log (car.getName (), car.getType ());
